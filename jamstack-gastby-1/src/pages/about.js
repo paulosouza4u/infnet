d36688cf7c2724c284c9b309d7c84e4302917495
Paulo Souza <42076064+paulosouza4u@users.noreply.github.com>
2025-10-21 00:23:50 -0300

    update

---
+++ jamstack-gastby-1/src/pages/about.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+
+const AboutPage = () => {
+    return (
+        <main>
+            <h1>Sobre</h1>
+            <p>teste</p>
+        </main>
+    );
+}
+
+export const Head = () => <title>Sobre</title>
+
+export default AboutPage
---
+++ jamstack-gastby-2/src/pages/about.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+const AboutPage = () => {
+    return (
+        <main>
+            <h1>About Us</h1>
+            <p>Welcome to our about page!</p>
+        </main>
+    );
+}
+
+export const Head = () => <title>About</title>;
+
+export default AboutPage;
