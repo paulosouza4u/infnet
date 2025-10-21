a703fdc257516342e557317ac7431bc8f71febf0
Paulo Souza <42076064+paulosouza4u@users.noreply.github.com>
2025-10-21 20:40:57 -0300

    update

---
+++ jamstack-gastby_5/gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+      title: `Gatsby Default Starter`,
+    siteUrl: `https://googler.com.br`,
+  },
+  plugins: [],
+}
